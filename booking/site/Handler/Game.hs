11b1f2e9e2107ea5d03d39deaa154ed47a2d7d0f
lolo <lolo_vn@yahoo.co.uk>
2015-08-25 13:48:04 +0800

    Add routes for viewing game details and posting booking

---
+++ booking/site/Handler/Game.hs
@@ -0,0 +1,29 @@
+module Handler.Game where
+
+import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
+                              withSmallInput)
+
+getGameR :: GameId -> Handler Html
+getGameR gameId = do
+    Just game <- runDB $ do
+        get gameId
+    (formWidget, formEnctype) <- generateFormPost sampleForm
+    defaultLayout $ do
+        setTitle "Locked Danang"
+        $(widgetFile "game")
+
+postBookingR :: Handler Html
+postBookingR = do
+    (formWidget, formEnctype) <- generateFormPost sampleForm
+    let submission = Nothing :: Maybe (FileInfo, Text)
+        handlerName = "getHomeR" :: Text
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "homepage")
+
+sampleForm :: Form (FileInfo, Text)
+sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
+    <$> fileAFormReq "Choose a file"
+    <*> areq textField (withSmallInput "What's on the file?") Nothing
