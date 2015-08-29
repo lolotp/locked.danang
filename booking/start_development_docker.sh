eed215d00176f076cb727b2393ffba24aa453884
lolo <lolo_vn@yahoo.co.uk>
2015-08-30 03:05:45 +0800

    Update development server starting script

--- booking/start_development_docker.sh
+++ booking/start_development_docker.sh
@@ -2,4 +2,4 @@
 
 # replace booking_database_run_1 with appropriate container name when running the database
 sudo docker-compose run -d database
-sudo docker run -p 3000:3000 -itv ~/locked.danang/booking/site:/opt/server --link booking_database_run_1:database -e "HOST=0.0.0.0" -e "PGHOST=database" -e "PGPORT=5432" -e "PGUSER=postgres" -e "PGDATABASE=postgres" lolotp/yesod /bin/bash
+sudo docker run -p 3000:3000 -itv ~/locked.danang/booking/site:/opt/server --link booking_database_run_1:database -e "HOST=0.0.0.0" -e "PGHOST=database" -e "PGPORT=5432" -e "PGUSER=postgres" -e "PGDATABASE=postgres" -e "FB_APPID=fill_in" -e "FB_APPSECRET=fill_in" lolotp/yesod /bin/bash
